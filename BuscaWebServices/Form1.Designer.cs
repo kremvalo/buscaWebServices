48fb79a11aabee471467745f9764ea23be1f10d2
krenthinker <kren.thinker@gmail.com>
2018-05-20 12:30:28 -0500

    Subida de de proyecto

---
+++ BuscaWebServices/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace BuscaWebServices
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtRuta = new System.Windows.Forms.TextBox();
+            this.txtNombreTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtRutaArchivo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(132, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtRuta
+            // 
+            this.txtRuta.Location = new System.Drawing.Point(12, 73);
+            this.txtRuta.Name = "txtRuta";
+            this.txtRuta.Size = new System.Drawing.Size(303, 20);
+            this.txtRuta.TabIndex = 1;
+            // 
+            // txtNombreTxt
+            // 
+            this.txtNombreTxt.Location = new System.Drawing.Point(12, 34);
+            this.txtNombreTxt.Name = "txtNombreTxt";
+            this.txtNombreTxt.Size = new System.Drawing.Size(303, 20);
+            this.txtNombreTxt.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre archivo de texto de la lista";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ruta de dominio";
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.Location = new System.Drawing.Point(84, 166);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(0, 13);
+            this.lblMsg.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(142, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ruta para guardar el archivo";
+            // 
+            // txtRutaArchivo
+            // 
+            this.txtRutaArchivo.Location = new System.Drawing.Point(13, 117);
+            this.txtRutaArchivo.Name = "txtRutaArchivo";
+            this.txtRutaArchivo.Size = new System.Drawing.Size(302, 20);
+            this.txtRutaArchivo.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 199);
+            this.Controls.Add(this.txtRutaArchivo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombreTxt);
+            this.Controls.Add(this.txtRuta);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtRuta;
+        private System.Windows.Forms.TextBox txtNombreTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblMsg;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtRutaArchivo;
+    }
+}
+
---
+++ BuscaWebServices/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BuscaWebServices
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> listSitesServices = new List<string>();
+            List<string> listSitesETBLIB = new List<string>();
+            //string rutaServidor = "G:/inetpub/wwwroot/etbserver/";
+            string rutaServidor = txtRuta.Text;
+
+            List<string> urlSitiosServer = Directory.GetDirectories(rutaServidor).ToList();
+
+            foreach (var sitio in urlSitiosServer)
+            {
+                List<string> carpetasSitio = Directory.GetDirectories(sitio).ToList();
+                foreach (var item in carpetasSitio)
+                {
+                    if (item.Contains("Web References") || item.Contains("Service References"))
+                    {
+                        string ruta="";
+                        if (item.Contains("Web References"))
+                        {
+                            ruta = item.Replace("Web References", "");
+                        }
+                        else if (item.Contains("Service References"))
+                        {
+                            ruta = item.Replace("Service References", "");
+                        }
+                        listSitesServices.Add((ruta.Replace(rutaServidor, "")).Replace("\\",""));
+                    }
+                    if (item.Contains("bin")) {
+                        if (File.Exists(item + "/ETB.Lib.dll")) {
+                            listSitesETBLIB.Add(item.Replace(rutaServidor, ""));
+                        }
+                    }
+                }
+            }
+
+            listSitesServices = listSitesServices.Distinct().ToList();
+            listSitesServices = listSitesServices.Union(listSitesETBLIB).ToList();
+
+            string rutaTxt = txtRutaArchivo + txtNombreTxt.Text + ".txt";
+            using (StreamWriter writer = new StreamWriter(rutaTxt))
+            {
+                foreach (var item in listSitesServices)
+                {
+                    writer.WriteLine(item);
+                }
+            }
+
+            lblMsg.Text = "Listo " + txtNombreTxt.Text;
+        }
+    }
+}
